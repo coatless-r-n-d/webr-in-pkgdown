3028e899a0f80abbf3c72460e87810c8f1eeead8
james.balamuta@gmail.com <james.balamuta@gmail.com>
2025-07-08 01:39:22 -0700

    Roll website build

---
+++ R/in-webr.R
@@ -0,0 +1,11 @@
+#' Detect if the version of R is built using Web Assembly (WASM)
+#' 
+#' The function checks whether the compiled version of _R_ shows an architecture
+#' that matches `"emscripten"` . If it's the case, we're likely using webR to access
+#' R.
+#' 
+#' @export
+#' @examplesWebR
+#' # Check to see if WASM is active
+#' in_webr()
+in_webr <- function() { R.Version()$os == "emscripten" }
---
+++ R/residual_surrealism.R
@@ -0,0 +1,33 @@
+#' Residual (Sur)Realism
+#'
+#' A dataset that yields a model with surreal residuals.
+#'
+#' @format 
+#' A data frame with 5,395 rows and 7 columns:
+#' \describe{
+#'   \item{V1}{Target variable of interest}
+#'   \item{V2}{First predictor variable}
+#'   \item{V3}{Second predictor variable}
+#'   \item{V4}{Third predictor variable}
+#'   \item{V5}{Fourth predictor variable}
+#'   \item{V6}{Fiveth predictor variable}
+#'   \item{V7}{Sixth predictor variable}
+#' }
+#' 
+#' @examplesWebR
+#' # Load the data
+#' data(residual_surrealism)
+#' 
+#' # Fit a model
+#' model = lm(V1 ~ ., residual_surrealism)
+#' 
+#' # Graph the residuals vs. fitted values
+#' plot(fitted(model),resid(model))
+#' 
+#' # Add a line at y = 0
+#' abline(0, 0, col = "red")
+#' 
+#' @source 
+#' Leonard A Stefanski
+#' <https://www.tandfonline.com/doi/abs/10.1198/000313007X190079>
+"residual_surrealism"
